8df5a9f76ea718e7c34b11f329a705c3d4e2ca00
Alieu Ndimbalane <alieu.itg@gmail.com>
2022-02-07 23:35:23 +0000

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { BiSearch } from 'react-icons/bi'
+import { HiCheckCircle, HiOutlineStar, HiStar } from 'react-icons/hi'
+import logo from '../assets/logo.svg';
+
+const Header: React.FC = () => {
+  return(
+    <header>
+      <img src={logo} alt="Logo" />
+      <form className="shadow-lg">
+        <input type="text" placeholder="Search the web without being tracked" />
+        <button type="submit">
+          <BiSearch size={24} color="#a5a5a5" />
+        </button>        
+      </form>
+      <h1>Tired of being tracked online? We can help.</h1>
+      <p className="subtitle">Get seamless privacy protection on your browser for free with one download:</p>
+      <div className="actions">
+        <p>
+          <HiCheckCircle size={20} color="blue"/>
+          <span>Private Search</span>
+        </p>
+        <p>
+          <HiCheckCircle size={20} color="blue"/>
+          <span>Tracker Blocking</span>
+        </p>
+        <p>
+          <HiCheckCircle size={20} color="blue"/>
+          <span>Site Encryption</span>
+        </p>
+      </div>    
+      <p className="rating">
+        <HiStar size={15} color="#a5a5a5"/>
+        <HiStar size={15} color="#a5a5a5"/>
+        <HiStar size={15} color="#a5a5a5"/>
+        <HiStar size={15} color="#a5a5a5"/>
+        <HiOutlineStar size={15} color="#a5a5a5"/>
+        <span>Rated: 4.0/5</span>
+      </p>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { HiOutlineSpeakerphone, HiOutlineChevronDown,HiOutlineMenu } from 'react-icons/hi'
+
+const Navbar: React.FC = () => {
+  return(
+    <nav>
+      <ul className="navbar bg-red-500">
+        <li>
+          <button className="flex items-center">
+            <HiOutlineSpeakerphone size={24} color="#a5a5a5"/>
+            <HiOutlineChevronDown size={20} color="#a5a5a5"/>
+          </button>
+        </li>
+        <li>
+          <button>
+            <HiOutlineMenu size={24} color="#a5a5a5"/>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+ export default Navbar
